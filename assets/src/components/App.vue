899827cb004487074eef7f5f944e106d14f4eb5a
Lutfur Rahman <46296850+Lutfur-info@users.noreply.github.com>
2022-10-02 13:02:16 +0600

    Create App.vue

---
+++ assets/src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <todo-list />
+    </div>
+</template>
+
+<script>
+    import TodoList from "./TodoList"
+
+    export default {
+        name: 'app',
+        components: {
+            TodoList
+        }
+    }
+</script>
